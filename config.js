542581122b81e242ffdf33a6538179b491072611
chaoyuu <chaoyu995@gmail.com>
2021-08-14 21:19:57 +1000

    conenct firebase

---
+++ config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyCwmUPC5YuBo8CCwvWpo4J57LPlQvuIAGw",
+  
+    authDomain: "fianl-app.firebaseapp.com",
+  
+    projectId: "fianl-app",
+  
+    storageBucket: "fianl-app.appspot.com",
+  
+    messagingSenderId: "726550337517",
+  
+    appId: "1:726550337517:web:2c9d5cc94413ac549639e4",
+
+}
