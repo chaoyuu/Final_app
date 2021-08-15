115d0abf00837b352511c8404c9705cddd306766
chaoyuu <chaoyu995@gmail.com>
2021-08-16 02:13:00 +1000

    add new screen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -10,72 +10,130 @@ import * as firebase from 'firebase'
 import { Signup } from './components/Signup';
 import { Signin } from './components/Signin';
 import { Home } from './components/Home';
+import { Profile } from './components/Profile';
+import { Detail } from './components/Detail';
 
-if( !firebase.apps.length ) {
-  firebase.initializeApp( firebaseConfig )
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
 }
 
 export default function App() {
-  const [ auth, setAuth ] = useState( false )
-
+  const [auth, setAuth] = useState(null)
+  const [data, setData] = useState()
+  const db = firebase.firestore()
   // useEffect( () => {
   //   if( auth ) {
   //     navigation.navigate("Home")
   //   }
   // }, [auth])
-  firebase.auth().onAuthStateChanged( (user) => {
-    if( user ) {
-      setAuth( true )
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      setAuth(true)
     }
     else {
-      setAuth( false )
+      setAuth(false)
     }
   })
 
-  const HandleSignup = ( email, password ) => {
-    // console.log( email, password )
-    firebase.auth().createUserWithEmailAndPassword( email, password )
-    .then( (response) => {
-      console.log(response)
-      setAuth( true )
-    } )
-    .catch( (error) => console.log(error) )
-  }
-
-  const HandleSignin = ( email, password ) => {
-    firebase.auth().signinWithEmainAndPassword( email, password )
-    .then( (response) => {
-      console.log(response)
-      setAuth( true )
+  useEffect(() => {
+    if (!data) {
+      readData()
+    }
+  })
+  const addData = (data) => {
+    return new Promise((resolve, reject) => {
+      if (!auth) {
+        reject("User not authenticated")
+      }
+      else {
+        const ref = db.collection('users').doc(auth.uid).collection('read')
+        ref.add(data)
+          .then(() => { resolve(true) })
+          .catch((error) => { reject(error) })
+      }
     })
-    .catch( (error) => console.log(error) )
   }
 
-  const ToggleSignup = () => {
-    if( signup === true ) {
-      setSignup( false )
-    }
-    else {
-      setSignup( true )
-    }
+  const HandleSignup = (email, password) => {
+    // console.log( email, password )
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log(response)
+        setAuth(true)
+      })
+      .catch((error) => console.log(error))
   }
-  
+
+  const HandleSignout = () => {
+    return new Promise((resolve, reject) => {
+      firebase.auth().signOut()
+        .then(() => {
+          setAuth(false)
+          resolve(true)
+
+        })
+        .catch((error) => {
+          console.log(error)
+          reject(error)
+        })
+    })
+  }
+  const readData = () => {
+    let books = []
+    db.collection('books').get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          let book = doc.data()
+          book.id = doc.id
+          books.push(book)
+        })
+      })
+    setData(books)
+  }
+  const HandleSignin = (email, password) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log(response)
+        setAuth(true)
+      })
+      .catch((error) => console.log(error))
+  }
+
+  //const ToggleSignup = () => {
+  //if( signup === true ) {
+  //  setSignup( false )
+  // }
+  // else {
+  //   setSignup( true )
+  // }
+  // }
+
   const Stack = createStackNavigator()
 
-  return(
+  return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Signup">
         <Stack.Screen name="Signin" >
-          { (props) => <Signin {...props} handler={HandleSignin} auth={auth}/>}
+          {(props) => <Signin {...props} handler={HandleSignin} auth={auth} />}
         </Stack.Screen>
         <Stack.Screen name="Signup">
-          { (props) => <Signup {...props} handler={HandleSignup} auth={auth}/>}
+          {(props) => <Signup {...props} handler={HandleSignup} auth={auth} />}
         </Stack.Screen>
-        <Stack.Screen 
-        name="Home" 
-        component={Home}
-        options={ {title: "Demo App"} }
-        />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Detail" component={Detail} />
+
+
+        <Stack.Screen
+          name="Home"
+          //component={Home}
+          options={{ title: "Borrow Record  App" }}
+        >
+          {(props) => <Home {...props}
+            signout={HandleSignout} listdata={data}
+            auth={auth} 
+            add={addData} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
@@ -90,3 +148,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
