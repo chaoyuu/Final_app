5a6b1709abfd6712d5e6501dbeb9c8802af9fb86
chaoyuu <chaoyu995@gmail.com>
2021-08-16 02:15:07 +1000

    update new functions for logout

---
+++ components/Logout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export function Logout( props ) {
+  const PressHandler = () => {
+    props.handler()
+  }
+  return (
+    <TouchableOpacity style={styles.button} onPress={ PressHandler}>
+      <Text>Sign Out</Text>
+    </TouchableOpacity>
+    
+  )
+}
+
+const styles = StyleSheet.create( {
+  button : {
+    backgroundColor: "blue",
+  }
+})
